3191354913bc7f7af7e9c0f4b1eeaf63fd280816
Jeff Smith <jeff.smith@forescout.com>
2024-03-18 17:13:33 -0400

    bump version v30.0.0rc0 -> v30.0.0rc1 [skip-ci]

--- gojira/__init__.py
+++ gojira/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'BatCave'
 __summary__ = 'Python Programming Toolkit'
 __uri__ = 'https://github.com/tardis4500/gojira/'
 
-__version__ = '30.0.0rc0'
+__version__ = '30.0.0rc1'
 __build_name__ = '{var:build_name}'
 __build_date__ = '{var:build_date}'
 
