ba139dc399958889d2d5cf8415653f1825381b9d
tardis4500 <tardis4500@users.noreply.github.com>
2024-03-21 00:30:32 +0000

    bump version v30.0.0rc50 -> v30.0.0rc51 [skip-ci]

--- vjer/__init__.py
+++ vjer/__init__.py
@@ -13,7 +13,7 @@ __title__ = 'Vjer'
 __summary__ = 'CI/CD Toolkit'
 __uri__ = 'https://github.com/tardis4500/vjer/'
 
-__version__ = '30.0.0rc50'
+__version__ = '30.0.0rc51'
 __build_name__ = '{var:build_name}'
 __build_date__ = '{var:build_date}'
 
